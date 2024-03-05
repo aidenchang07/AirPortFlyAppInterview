1e4756c1e9b56f51d3a70fa3b004cde50eb01a16
Aiden Chang <asdf62210@gmail.com>
2024-03-05 23:12:06 +0800

    Add AirPortFlyApplication & Dagger - Hilt

---
+++ app/src/main/java/com/adncoding/airportflyappinterview/AirPortFlyApplication.kt
@@ -0,0 +1,10 @@
+package com.adncoding.airportflyappinterview
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+/**
+ * Created by AidenChang 2024/03/04
+ */
+@HiltAndroidApp
+class AirPortFlyApplication : Application()
